44e6cde13a6c82186022e3ea32f5ad89a278b1f9
arsen2019 <arsen.grigoryan555@gmail.com>
2025-04-19 16:04:33 +0400

    Initial commit for face detection

---
+++ face_recognition.py
@@ -0,0 +1,52 @@
+import cv2
+import numpy as np
+import os 
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('trainer/trainer.yml')
+cascadePath = "haarcascade_frontalface_default.xml"
+
+faceCascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+id = 0
+names = ['None','Tagavor','None', 'No name'] # Dear Mari you have to put the names manually after the python main.py code running. This is a great opportunity to master some automation. The first one was trained by my pictures. 
+cam = cv2.VideoCapture(0)
+cam.set(3, 640) # set video widht
+cam.set(4, 480) # set video height
+minW = 0.1*cam.get(3)
+minH = 0.1*cam.get(4)
+
+while True:
+    ret, img =cam.read()
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    
+    faces = faceCascade.detectMultiScale( 
+        gray,
+        scaleFactor = 1.2,
+        minNeighbors = 5,
+        minSize = (int(minW), int(minH)),
+       )
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
+        if (confidence > 50):
+            id = names[id]
+            confidence = "  {0}%".format(round(confidence))
+        else:
+            id = "unknown"
+            confidence = "  {0}%".format(round(confidence))
+        
+        cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2 )
+        #cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
+    
+    cv2.imshow('camera',img) 
+    k = cv2.waitKey(10) & 0xff
+    if k == 27:
+        break
+print("\n [INFO] Exiting Program and cleanup stuff")
+cam.release()
+cv2.destroyAllWindows()
